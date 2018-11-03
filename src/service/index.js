d0820f44b0198f644492e2902f1c76a054b6ee58
Maciej Krüger <mkg20001@gmail.com>
2018-11-03 21:28:29 +0100

    feat: upstart

--- src/service/index.js
+++ src/service/index.js
@@ -4,6 +4,7 @@ const Wrapper = require('./wrapper')
 
 module.exports = {
   systemd: require('./systemd'),
+  upstart: require('./upstart'),
   linux: Wrapper(() => require('node-linux').Service, '/.pneumon-', 'linux'),
   mac: Wrapper(() => require('node-mac').Service, '/system/.pneumon-', 'darwing'),
   windows: Wrapper(() => require('node-windows').Service, 'C:\\windows\\pnemon-', 'win32')
